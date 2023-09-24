d30570fa9f2eeb4d584901ac7316f592b46977eb
carraes <carraeshb@gmail.com>
2023-09-24 19:24:16 -0300

    feat: make controller get gh service, async

--- server/src/main.py
+++ server/src/main.py
@@ -1,11 +1,10 @@
-import requests
-from deps.get_api_key import get_api_key
 from fastapi import Depends, FastAPI
 from mangum import Mangum
+from src.deps.get_api_key import get_api_key
+from src.factories.get_gh_service import get_gh_service
+from src.services.gh import GitHubService
 
 app = FastAPI()
-
-baseURL = "https://api.github.com"
 
 
 @app.get("/")
@@ -13,13 +12,13 @@ def root():
     return {"message": "Hello World"}
 
 
-@app.get("/users/{user}")
-def entry(user: str, api_key: str = Depends(get_api_key)):
-    request_url = f"{baseURL}/users/{user}"
-
-    response = requests.get(request_url)
-
-    return response.json()
+@app.get("/users/{username}")
+async def entry(
+    username: str,
+    service: GitHubService = Depends(get_gh_service),
+    api_key: str = Depends(get_api_key),
+):
+    return await service.get_repos(username)
 
 
 handler = Mangum(app)
