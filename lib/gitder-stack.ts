bdb318b5283d18544ba400286a8d9b7dfca2b13f
carraes <carraeshb@gmail.com>
2023-10-01 20:21:45 -0300

    feat: create dynamo db on cdk

--- lib/gitder-stack.ts
+++ lib/gitder-stack.ts
@@ -2,15 +2,24 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
 export class GitderStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const table = new dynamodb.Table(this, "ReposTable", {
+      partitionKey: { name: "full_name", type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
     const dockerFunc = new lambda.DockerImageFunction(this, "DockerFunc", {
       code: lambda.DockerImageCode.fromImageAsset("./server"),
       memorySize: 1024,
       timeout: cdk.Duration.seconds(10),
+      environment: {
+        DYNAMO_TABLE_NAME: table.tableName,
+      },
     });
 
     const secret = secretsmanager.Secret.fromSecretNameV2(
@@ -19,6 +28,7 @@ export class GitderStack extends cdk.Stack {
       "GITHUB_KEY",
     );
 
+    table.grantReadWriteData(dockerFunc);
     secret.grantRead(dockerFunc);
 
     const functionUrl = dockerFunc.addFunctionUrl({
